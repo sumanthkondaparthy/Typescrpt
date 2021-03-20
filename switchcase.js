1eb46232b3c938c02af9702410d3a218f69c7b3e
shashikanthredy <shashikanthreddyg@gmail.com>
2021-03-20 15:26:12 +0530

    Switch Case Commit

---
+++ switchcase.js
@@ -0,0 +1,12 @@
+var a = 1;
+switch (a) {
+    case 1:
+        document.write("hi one");
+        break;
+    case 2:
+        document.write("hi two");
+        break;
+    case 2:
+        document.write("hi three");
+        break;
+}
